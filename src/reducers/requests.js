b4b81eb8f025f8086560092a19e02bbc8dec9989
aaron-schuyler <asky28@gmail.com>
2020-09-23 15:49:52 -0400

    setup file structure and started building out reducers for redux

---
+++ src/reducers/requests.js
@@ -0,0 +1,20 @@
+export default (state = [], action) => {
+  let requests = [...state]
+  switch (action.type) {
+    case 'ADD_REQUEST':
+      requests.push(action.ticket)
+      return requests
+    case 'ACCEPT_REQUEST':
+      //if ticket.accepted == false or if ticket.status == 'conditional_pending'
+      //change ticket.accepted to true and accepted_by to user.id and ticket.status to 'pending' submit to API
+      return requests
+    case 'REJECT_REQUEST':
+      //Remove Join Table Entry connecting ticket and USer
+      return requests
+    case 'CONDITIONALLY_ACCEPT_REQUEST':
+      //add note to ticket and change status to 'conditional_pending' and set accepted_by to user.id
+      return requests
+    default:
+      return requests
+  }
+}
---
+++ src/reducers/tickets.js
@@ -0,0 +1,27 @@
+export default (state = [], action) => {
+  let tickets = [...state]
+  switch (action.type) {
+    case 'ADD_TICKET':
+      tickets.push(action.ticket)
+      return tickets
+    case 'MODIFY_TICKET':
+      //update ticket
+      return tickets
+    case 'SUBMIT_TICKET':
+      //change ticket status to 'submitted'
+      return tickets
+    case 'DELETE_TICKET':
+      //Delete Ticket and remove from state
+      return tickets
+    case 'APPROVE_TICKET':
+      //can only be done if ticket.status is pending or conditional_pending
+      //remove all ticket user join table entries except the one that has the matching user ID
+      //set status to approved
+      return tickets
+    case 'CLOSE_TICKET':
+      //set ticket.status to 'closed'
+      return tickets
+    default:
+      return tickets
+  }
+}
