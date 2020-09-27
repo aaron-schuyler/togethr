af3a38f715f54bfebea4265141a28a6e9b6b4a55
aaron-schuyler <asky28@gmail.com>
2020-09-27 01:27:55 -0400

    made request card

--- src/components/request.js
+++ src/components/request.js
@@ -2,8 +2,28 @@ import React from 'react'
 
 export default function Request(props) {
   return (
-    <>
-      {props.request.title}
-    </>
+    <div className='card'>
+      <div className='card-header'>
+        <h2>{props.request.title}</h2>
+        <span className='badge border text-primary border-primary'>{props.request.category}</span>
+        <b> - </b>
+        <span className='badge border text-secondary border-secondary'>{props.request.subcategory}</span>
+        <br />
+        <hr />
+        <b>Skills: </b>{props.request.skills.map((skill, i) => {
+          return <span key={i} className='badge badge-pill badge-secondary'>{skill}</span>
+        })}
+      </div>
+      <div className='card-body'>
+        <p>{props.request.description}</p>
+      </div>
+      <div className='card-footer text-center'>
+        <div className='btn-group'>
+          <button className='btn-sm btn btn-success'>Accept</button>
+          <button className='btn-sm btn btn-danger'>Decline</button>
+        </div>
+
+      </div>
+    </div>
   )
 }
